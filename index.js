0cfcb87993453bc21c8c1ecfab25396d8cd41986
rakib hasan <rakibhasanmd457@gmail.com>
2025-05-20 23:35:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function chabanao() {
+    return new Promise(function (resolved, reject) {
+        console.log("cha banano suru hoilo");
+
+        setTimeout(function () {
+            let chahoise = true;
+
+            if (chahoise) {
+                resolved("successfully done",);
+            }
+            else {
+                reject('hoi nai akhn oo')
+            }
+
+        }, 3000)
+    })
+}
+
+chabanao()
+    .then(function (result) {
+        console.log("success", result);
+    })
+    .catch(function (error) {
+        console.log("hbe na", error);
+    })
